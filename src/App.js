afbeccfe1af9fa8be8a76771a98ed9bc94063e70
Nevada <freegasss@gmail.com>
2024-06-03 16:35:29 +1100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import CardTitle from "./components/CardTitle";
+import SpecialFileTreatment from "./components/SpecialFileTreatment";
+import "bootstrap/dist/css/bootstrap.min.css";
+import cat from './cat.jpg';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+        <CardTitle img={cat} />
+        <br />
+        <SpecialFileTreatment />
+      </div>
   );
 }
 
